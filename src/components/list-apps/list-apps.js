a8f0dbc448fab73a63d918fa41594b91edad62b1
pajaga <37742247+pajaga@users.noreply.github.com>
2020-04-21 15:40:42 +0200

    fetch templates

--- src/components/list-apps/list-apps.js
+++ src/components/list-apps/list-apps.js
@@ -1,88 +1,129 @@
 import React, { useEffect, useState } from "react";
-import Card from "react-bootstrap/Card";
+// import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Media from "react-bootstrap/Media";
 import { Row, Col, Container, ButtonGroup } from "react-bootstrap";
 import "./list-apps.css";
+// import TemplateService from "../../services/template-service";
 
 export default function ListApps() {
-//   const [error, setError] = useState(null);
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+  const jwt = ("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsInJvbGUiOjEsImV4cCI6MTU4NzQ5OTEyMn0.1ioF6xOHgJMPoI86IeW871j0uTKPfyclBEci5qYe-vQ");
 
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer " + jwt);
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
-    // fetch("https://api.example.com/items")
-    //   .then((res) => res.json())
-    //   .then(
-    //     (result) => {
-    //       setIsLoaded(true);
-    //       setItems(result.items);
-    //     },
-    //     // Note: it's important to handle errors here
-    //     // instead of a catch() block so that we don't swallow
-    //     // exceptions from actual bugs in components.
-    //     (error) => {
-    //       setIsLoaded(true);
-    //       setError(error);
-    //     }
-    //   );
+    // fetch("http://141.72.191.81:9000/api/templates", requestOptions)
+    fetch("http://192.168.0.55:3001/api/templates", requestOptions)
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true);
+        setItems(result.items);
+      },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
   }, []);
 
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   } else if (!isLoaded) {
-//     return <div>Loading...</div>;
-//   } else {
-    // return (
-    //   <ul>
-    //     {items.map((item) => (
-    //       <li key={item.name}>
-    //         {item.name} {item.price}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // );
-//   }
-    return sampleData.map((item) => (
-      <Media className="app-list-item">
-        <img
-          width={64}
-          height={64}
-          className="align-self-center mr-3"
-          src={item.logo}
-          alt={item.title}
-        />
-        <Media.Body className="app-list-media-body">
-          <Container fluid>
-            <Row>
-              <Col sm={10}>
-                <h5>{item.title}</h5>
-                <p className="app-list-item-description">{item.description}</p>
-              </Col>
-              <Col sm={2}>
-                <ButtonGroup
-                  vertical
-                  size="sm"
-                  aria-label="app-list-actions"
-                  className="app-list-media-buttons"
-                >
-                  <Button variant="primary" size="sm">
-                    Show
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+        items.map(item => (
+          <Media className="app-list-item">
+            <img
+              width={64}
+              height={64}
+              className="align-self-center mr-3"
+              src={item.logo}
+              alt={item.title}
+            />
+            <Media.Body className="app-list-media-body">
+              <Container fluid>
+                <Row>
+                  <Col sm={10}>
+                    <h5>{item.title}</h5>
+                    <p className="app-list-item-description">{item.description}</p>
+                  </Col>
+                  <Col sm={2}>
+                    <ButtonGroup
+                      vertical
+                      size="sm"
+                      aria-label="app-list-actions"
+                      className="app-list-media-buttons"
+                    >
+                      <Button variant="primary" size="sm">
+                        Show
                   </Button>
-                  <Button variant="success" size="sm">
-                    Get App
+                      <Button variant="success" size="sm">
+                        Get App
                   </Button>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Container>
-        </Media.Body>
-      </Media>
-    ));
+                    </ButtonGroup>
+                  </Col>
+                </Row>
+              </Container>
+            </Media.Body>
+          </Media>
+        ))
+    );
+  }
+
+
+    // return sampleData.map((item) => (
+    //   <Media className="app-list-item">
+    //     <img
+    //       width={64}
+    //       height={64}
+    //       className="align-self-center mr-3"
+    //       src={item.logo}
+    //       alt={item.title}
+    //     />
+    //     <Media.Body className="app-list-media-body">
+    //       <Container fluid>
+    //         <Row>
+    //           <Col sm={10}>
+    //             <h5>{item.title}</h5>
+    //             <p className="app-list-item-description">{item.description}</p>
+    //           </Col>
+    //           <Col sm={2}>
+    //             <ButtonGroup
+    //               vertical
+    //               size="sm"
+    //               aria-label="app-list-actions"
+    //               className="app-list-media-buttons"
+    //             >
+    //               <Button variant="primary" size="sm">
+    //                 Show
+    //               </Button>
+    //               <Button variant="success" size="sm">
+    //                 Get App
+    //               </Button>
+    //             </ButtonGroup>
+    //           </Col>
+    //         </Row>
+    //       </Container>
+    //     </Media.Body>
+    //   </Media>
+    // ));
 
 }
 
